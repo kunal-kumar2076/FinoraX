9aaa51fa3e4be503e6f91c1266c33af6aeb6bae2
kunal-kumar2076 <kunalkr2076@gmail.com>
2026-01-15 12:11:50 +0530

    Update sell order API endpoint to production URL

--- dashboard/src/components/SellActionWindow.js
+++ dashboard/src/components/SellActionWindow.js
@@ -13,7 +13,7 @@ const SellActionWindow = ({ uid }) => {
   const [stockPrice, setStockPrice] = useState(0);
 
   const handleSellClick = () => {
-    axios.post('http://localhost:3002/sellOrder',{
+    axios.post('https://finorax-backend-b650.onrender.com',{
         name : uid,
         qty : stockQuantity,
         price : stockPrice,
